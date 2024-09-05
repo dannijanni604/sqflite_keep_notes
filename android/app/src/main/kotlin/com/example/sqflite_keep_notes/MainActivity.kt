6751c1abdcc99cd1038e2c15bd46bfbe9c00656c
adnan-wb <aaservices604@gmail.com>
2024-09-05 11:06:11 +0500

    initial-commit

---
+++ android/app/src/main/kotlin/com/example/sqflite_keep_notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sqflite_keep_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_keep_notes/views/home/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeView());
+  }
+}
